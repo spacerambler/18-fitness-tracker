5ebffdc069392f76479e109be89eeb0c91d13840
Keely Bailey <keelybailey3@gmail.com>
2021-09-08 16:39:00 -0500

    Think through next routes

--- app/router.js
+++ app/router.js
@@ -8,8 +8,16 @@ router.get("/workouts", async (_,res)=>{
         const workouts = await controller.index();
     res.json(workouts);
     } catch (err) {console.log(err);}
-    
+})
 
+router.post("/workouts", async (req, res)=> {
+    try {
+        const newWorkout = await controller.create(req.body);
+        res.status(201).json(newWorkout);
+    } catch(error) {
+        res.status(400).json(error.message);
+        console.log(err);
+    }
 })
 
 export default router;
